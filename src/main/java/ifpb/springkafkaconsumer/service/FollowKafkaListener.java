f7999a742439c391ae30d7b592b2383ff382d08b
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-08-15 11:06:48 -0300

    Criando repository

--- src/main/java/ifpb/springkafkaconsumer/service/FollowKafkaListener.java
+++ src/main/java/ifpb/springkafkaconsumer/service/FollowKafkaListener.java
@@ -1,15 +1,21 @@
 package ifpb.springkafkaconsumer.service;
 
+import ifpb.springkafkaconsumer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowKafkaListener {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @KafkaListener(topics="${kafka.topic.follow}", groupId = "follow-listener-group",
             containerFactory = "kafkaListenerContainerFactory")
     public void listen(String event){
         System.out.println(event);
+
     }
 
 }
